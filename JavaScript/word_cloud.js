142a625a5ba0e1dc2154e25794eff86d9968e491
shenmemingzine <rongwingyu@gmail.com>
2019-05-31 13:50:23 -0700

    Update word_cloud.js

--- JavaScript/word_cloud.js
+++ JavaScript/word_cloud.js
@@ -1,13 +1,11 @@
-var vocabs = ["Computer Science", "Electrical Engineering", "Mechanical Engineering",
-              "Chemical Engineering", "Industrial Engineering", "Information Technology", 
-              "Civil Engineering", "Statistics", "Nursing", "Management Information Systems", 
-              "Finance", "Mathematics", "Biomedical Engineering", "Accounting", "Economics", "Physics", "Biotechnology", "Architecture", "Fashion Design", "Business",
-              "International Relations", "Graphic Design", "Marketing", "English", "Political Science",
-              "History", "Human Resources", "Journalism", "Advertising", "Philosophy"];
+var vocabs = ["Python", "MatLab", "SQL", "LaTeX", "Microsoft Office", "HTML",
+							"CSS", "JavaScript", "Keras", "PyTorch", "Matplotlib", "Seaborn",
+              "Plotly", "Beautiful Soup", "Machine Learning", "Statistics", 
+              "Applied Mathematics", "Natural Language Processing"];
               
 var margin = {top: 10, right: 10, bottom: 10, left: 10};
-var width = 300 - margin.left - margin.right;
-var height = 150 - margin.top - margin.bottom;
+var width = 450 - margin.left - margin.right;
+var height = 250 - margin.top - margin.bottom;
 var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 var svg = d3.select("#word-cloud")
@@ -24,8 +22,8 @@ var svg = d3.select("#word-cloud")
    
 var layout = d3.layout.cloud()
                .size([width, height])
-               .words(vocabs.map(function(d) { return {text: d, size: 5+Math.random()*15}; }))
-               .padding(5)
+               .words(vocabs.map(function(d) { return {text: d, size: 15+Math.random()*(vocabs.length+1)}; }))
+               .padding(0)
                .rotate(function() { return ~~(Math.random()*2)*45; })
                .font("Impact")
                .fontSize(function(d) { return d.size; })
@@ -47,4 +45,3 @@ function draw(words) {
        .attr("transform", function(d) { return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";})
        .text(function(d) { return d.text; });
 };
-
